8d4ed2f2ae4e06ad10bef8298edc38c098324825
Thomas MacLean <thomas.maclean@gmail.com>
2019-08-13 11:44:22 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const superb = require('superb');
 const badwordsArray = require('badwords/array');
 const placeholder = document.getElementById('password');
 
-console.log('starting 🚀');
+console.log('🚗 starting 🚀');
 const createPassword = () => {
   const badWord =
     badwordsArray[Math.floor(Math.random() * badwordsArray.length)];
